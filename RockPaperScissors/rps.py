86de440ce150841b4bc84b1cc95b4fa9a75d97e9
Jamal Hawkins <54287100+Jamal-prog@users.noreply.github.com>
2022-04-07 12:00:20 -0500

    format update

--- RockPaperScissors/rps.py
+++ RockPaperScissors/rps.py
@@ -11,6 +11,7 @@ def runGame():
     # create a random index to get cpu answer
     randIdx = random.randint(0, 2)
 
+    # create a default starting score
     playerScore = 0 
 
     # grab random index from choices
@@ -26,37 +27,80 @@ def runGame():
         print("ROCK")
         print("PAPER")
         print("SCISSORS")
-        print("********")
+        print("")
         # Conditions
         if playerChoice == "rock" and cpuChoice == "scissors":
             print(f"Player Choice: {playerChoice} , CPU choice {cpuChoice}, You won!!")
+            rockImg()
+            scissorsImg()
             playerScore += 1
          
-            
         elif playerChoice == "paper" and cpuChoice == "rock":
             print(f"Player Choice: {playerChoice} , CPU choice: {cpuChoice}, You won!!") 
+            paperImg()
+            rockImg()
             playerScore += 1
-           
            
         elif playerChoice == "scissors" and cpuChoice == "paper":
             print(f"Player Choice: {playerChoice} , CPU choice: {cpuChoice}, You won!!")
+            scissorsImg()
+            paperImg()
             playerScore += 1
            
-         
+        elif playerChoice == cpuChoice:
+            print(f"Player Choice: {playerChoice}, CPU choice: {cpuChoice}, There is a tie!")
         else:
             print(f"Player Choice: {playerChoice} , CPU choice: {cpuChoice}, Sorry, try again!")    #loser handling
             playerScore -= 1
             if playerScore < 0:
                 playerScore=0
-      
-  
+
         # any input error handling
     else:
         print("Please pick a valid choice: Rock, Paper, or scissors")
+        rockImg()
+        paperImg()
+        scissorsImg()
 
     return playerScore
     
+def rockImg():
+    # ascii art for Rock     
+   print("""
+        _______
+    ---'   ____)
+          (_____) 
+          (_____)
+          (____)
+    ---.__(___)
+    """)
+
+def paperImg():
+    # paper ascii art
+    print("""
+        _______
+    ---'    ____)____
+               ______)
+              _______)
+             _______)
+     ---.__________)
+    """)
+
+def scissorsImg():
+    # scissors ascii art
+    print("""
+        _______
+    ---'   ____)____
+              ______)
+           __________)
+          (____)
+    ---.__(___)
+    """)   
+
 # intro to game 
+rockImg()
+paperImg()
+scissorsImg()
 run = input("Welcome to Rock, Paper, Scissors, Do you wish to begin yes/no? ").lower()
 score = 0
 if run == "yes":
@@ -72,6 +116,8 @@ if run == "yes":
             break
         else:
             score += gameScore
+elif run == "no":
+    print("Restart game to continue")
       
      
 
